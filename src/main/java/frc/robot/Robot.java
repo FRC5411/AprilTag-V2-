b159ce544f7e3d4835ff54fbcfb015cb50347b3d
Nikhil4474 <ninuganti15@gmail.com>
2022-12-08 18:39:40 -0600

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,103 +23,60 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  // Constants such as camera and target height stored. Change per robot and goal!
+  final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
+  final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
+  // Angle between horizontal and the camera.
+  final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
 
-  private RobotContainer m_robotContainer;
+  // How far from the target we want to be
+  final double GOAL_RANGE_METERS = Units.feetToMeters(3);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  // Change this to match the name of your camera
+  PhotonCamera camera = new PhotonCamera("photonvision");
+
+  // PID constants should be tuned per robot
+  final double LINEAR_P = 0.1;
+  final double LINEAR_D = 0.0;
+  PIDController forwardController = new PIDController(LINEAR_P, 0, LINEAR_D);
+
+  final double ANGULAR_P = 0.1;
+  final double ANGULAR_D = 0.0;
+  PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
+
+  XboxController xboxController = new XboxController(0);
+
+  // Drive motors
+  PWMVictorSPX leftMotor = new PWMVictorSPX(0);
+  PWMVictorSPX rightMotor = new PWMVictorSPX(1);
+  DifferentialDrive drive = new DifferentialDrive(leftMotor, rightMotor);
+
   @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
-    // Creates UsbCamera and MjpegServer [1] and connects them
-    // UsbCamera usbCamera = new UsbCamera("USB Camera 0", 0);
-    // MjpegServer mjpegServer1 = new MjpegServer("serve_USB Camera 0", 1181);
-    // mjpegServer1.setSource(usbCamera);
+  public void teleopPeriodic() {
+      double forwardSpeed;
+      double rotationSpeed;
 
-    // // Creates the CvSink and connects it to the UsbCamera
-    // CvSink cvSink = new CvSink("opencv_USB Camera 0");
-    // cvSink.setSource(usbCamera);
+      forwardSpeed = -xboxController.getRightY();
 
-    // // Creates the CvSource and MjpegServer [2] and connects them
-    // CvSource outputStream = new CvSource("Blur", PixelFormat.kMJPEG, 640, 480, 30);
-    // MjpegServer mjpegServer2 = new MjpegServer("serve_Blur", 1182);
-    // mjpegServer2.setSource(outputStream);
-    CameraServer.startAutomaticCapture();
+      if (xboxController.getAButton()) {
+          // Vision-alignment mode
+          // Query the latest result from PhotonVision
+          var result = camera.getLatestResult();
+
+          if (result.hasTargets()) {
+              // Calculate angular turn power
+              // -1.0 required to ensure positive PID controller effort _increases_ yaw
+              rotationSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+          } else {
+              // If we have no targets, stay still.
+              rotationSpeed = 0;
+          }
+      } else {
+          // Manual Driver Mode
+          rotationSpeed = xboxController.getLeftX();
+      }
+
+      // Use our forward/turn speeds to control the drivetrain
+      drive.arcadeDrive(forwardSpeed, rotationSpeed);
   }
-
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
 }
